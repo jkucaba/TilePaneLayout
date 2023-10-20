85b74b6bc2b6c1cd9570cdf5d55a193646bae517
Jakub <jakub.kucaba@gmail.com>
2023-10-20 10:06:51 +0200

    Another Example

--- src/main/java/com/jakubku/tilepane/tilepanelayout/Main.java
+++ src/main/java/com/jakubku/tilepane/tilepanelayout/Main.java
@@ -11,9 +11,16 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        TilePane tileCenter = createTilePane(Pos.CENTER);
-        TilePane tileLeft = createTilePane(Pos.BOTTOM_LEFT);
-        HBox root = new HBox(10, tileCenter, tileLeft);
+        TilePane root = new TilePane(5,5);
+        Button b12 = new Button("One\nTwo");
+        Button b3 = new Button("Three");
+        Button b4 = new Button("Four");
+        Button b5 = new Button("Five");
+        Button b6 = new Button("Six");
+
+        TilePane.setAlignment(b3, Pos.BOTTOM_RIGHT);
+        root.getChildren().addAll(b12, b3, b4, b5, b6);
+        root.setPrefColumns(3);
         Scene scene = new Scene(root);
         stage.setTitle("StackPane Layout Example");
         stage.setScene(scene);
